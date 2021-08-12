2d0ff727a59005d94e2ec43318624cb8cc396dbb
unknown <k.pouria7@gmail.com>
2021-08-12 20:18:48 +0430

    Set Tools in StartUp And Add DbContext

---
+++ Identity/Data/DataBaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Identity.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Data
+{
+    public class DataBaseContext : IdentityDbContext<AppUser,AppRole,string> //IdentityDbContext<IdentityUser, IdentityRole, string> Default
+    {
+        public DataBaseContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+    }
+}
